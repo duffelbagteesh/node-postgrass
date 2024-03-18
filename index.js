b32029f8564ca42b894ded733a033ca4eda8562e
duffelbagteesh <leticia.tishy@gmail.com>
2024-03-18 16:06:23 -0400

    njk and endpoint for album and artists displays with songs

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
 const express = require('express');
 const nunjucks = require('nunjucks');
 
 const app = express();
 const port = process.env.PORT || 3000
+
+// configure pg
+const pg = require('pg')
+const client = new pg.Client({
+    connectionString: process.env.CONNECTION_STRING
+})
 
 // Configure Nunjucks
 nunjucks.configure('views', {
@@ -11,10 +18,25 @@ nunjucks.configure('views', {
     express: app
 });
 
+client.connect()
 
-app.get('/', (req, res) => {
+
+app.get('/', async (req, res) => {
+    let query = req.query.q
+    let results = []
+
+    if (query !== undefined) {
+        query = query.toLowerCase()
+        let likeQuery = `%${query}%`
+        results = await client.query(`SELECT track.name, album.title AS album_title, artist.name AS artist_name FROM track INNER JOIN album ON track.album_id = album.album_id INNER JOIN artist ON album.artist_id = artist.artist_id WHERE LOWER(track.name) LIKE $1`, [likeQuery]);
+    }
+
     // Render index.njk using the variable "title" 
-    res.render('index.njk', { title: "Welcome to my site!" });
+    res.render('search.njk', { title: "Search", query: query, rows: results.rows});
+})
+
+app.get('/artist/:id', async(req, res) => {
+
 })
 
 app.listen(port, () => {
